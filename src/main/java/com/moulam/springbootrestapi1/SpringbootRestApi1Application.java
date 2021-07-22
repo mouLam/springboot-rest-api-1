6052a476af6b2264590eeaa84e01a8059364832e
MBOUP MOUHAMADOU LAMINE p1922789 <mouhamadou-lamine.mboup@etu.univ-lyon1.fr>
2021-07-22 13:55:48 +0200

    first commit

---
+++ src/main/java/com/moulam/springbootrestapi1/SpringbootRestApi1Application.java
@@ -0,0 +1,13 @@
+package com.moulam.springbootrestapi1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootRestApi1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootRestApi1Application.class, args);
+    }
+
+}
