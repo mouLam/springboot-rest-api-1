910990daf9f33702334378954cde0cba570378c5
MBOUP MOUHAMADOU LAMINE p1922789 <mouhamadou-lamine.mboup@etu.univ-lyon1.fr>
2021-08-01 14:11:28 +0200

    Hash the Password

--- src/main/java/com/moulam/springbootrestapi1/Repositories/UserRepositoryImpl.java
+++ src/main/java/com/moulam/springbootrestapi1/Repositories/UserRepositoryImpl.java
@@ -2,6 +2,7 @@ package com.moulam.springbootrestapi1.Repositories;
 
 import com.moulam.springbootrestapi1.Exceptions.EtAuthException;
 import com.moulam.springbootrestapi1.Model.User;
+import org.mindrot.jbcrypt.BCrypt;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.jdbc.core.JdbcTemplate;
@@ -36,6 +37,9 @@ public class UserRepositoryImpl implements UserRepository {
 
     @Override
     public Integer create(String firstName, String lastName, String email, String password) throws EtAuthException {
+
+        String hashPassword = BCrypt.hashpw(password, BCrypt.gensalt(10));
+
         try {
             KeyHolder keyHolder = new GeneratedKeyHolder();
             jdbcTemplate.update(connection -> {
@@ -43,7 +47,7 @@ public class UserRepositoryImpl implements UserRepository {
                 preparedStatement.setString(1, firstName);
                 preparedStatement.setString(2, lastName);
                 preparedStatement.setString(3, email);
-                preparedStatement.setString(4, password);
+                preparedStatement.setString(4, hashPassword);
                 return preparedStatement;
             }, keyHolder);
             return (Integer) Objects.requireNonNull(keyHolder.getKeys()).get("USER_ID");
@@ -58,8 +62,7 @@ public class UserRepositoryImpl implements UserRepository {
 
         try {
             User user = jdbcTemplate.queryForObject(SQL_FIND_BY_EMAIL, new Object[]{email}, userRowMapper);
-            assert user != null;
-            if (!password.equals(user.getPassword()))
+            if (!BCrypt.checkpw(password, user.getPassword()))
                 throw new EtAuthException("Invalid email / password");
 
             return user;
