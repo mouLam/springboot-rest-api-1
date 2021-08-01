24f6a9328f33671878d9e15235d5281f45bab2bf
MBOUP MOUHAMADOU LAMINE p1922789 <mouhamadou-lamine.mboup@etu.univ-lyon1.fr>
2021-08-01 22:24:44 +0200

    API Postman collection

--- src/main/java/com/moulam/springbootrestapi1/SpringbootRestApi1Application.java
+++ src/main/java/com/moulam/springbootrestapi1/SpringbootRestApi1Application.java
@@ -1,10 +1,13 @@
 package com.moulam.springbootrestapi1;
 
 import com.moulam.springbootrestapi1.Filters.AuthFilter;
+import org.apache.catalina.filters.CorsFilter;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 @SpringBootApplication
 public class SpringbootRestApi1Application {
@@ -19,6 +22,20 @@ public class SpringbootRestApi1Application {
         AuthFilter authFilter = new AuthFilter();
         registrationBean.setFilter(authFilter);
         registrationBean.addUrlPatterns("/api/categories/*");
+        return registrationBean;
+    }
+
+    @Bean
+    public FilterRegistrationBean<CorsFilter> corsFilter(){
+        FilterRegistrationBean<CorsFilter> registrationBean = new FilterRegistrationBean<>();
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        CorsConfiguration corsConfiguration = new CorsConfiguration();
+        corsConfiguration.addAllowedOrigin("*");
+        corsConfiguration.addAllowedHeader("*");
+        source.registerCorsConfiguration("/**", corsConfiguration);
+        registrationBean.setFilter(new CorsFilter());
+        //registrationBean.setFilter(new CorsFilter(source));
+        registrationBean.setOrder(0);
         return registrationBean;
     }
 }
